af779612b77a8aabd3c46a56c2aed691561c44d0
Kevin Leung <kevinresol@gmail.com>
2020-03-11 22:33:16 +0800

    huh?

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,7 @@
 class Main {
 	static function main() {
 		var version = js.Lib.require('@actions/core').getInput('lix-version');
-		js.Lib.require('@actions/exec').exec('yarn global add lix@$version');
+		// js.Lib.require('@actions/exec').exec('yarn global add lix@$version');
+		Sys.command('yarn', ['global', 'add', 'lix@$version']);
 	}
 }
