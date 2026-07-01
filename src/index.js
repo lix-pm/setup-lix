0b705294362dcec12c9f6690b78dbb728faac45c
Kevin Leung <kevinresol@gmail.com>
2026-07-01 14:23:38 +1000

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { execSync } from "child_process";
+import * as core from "@actions/core";
 
 const version = core.getInput("lix-version");
 execSync(`npm install -g lix${version ? `@${version}` : ""}`, {
